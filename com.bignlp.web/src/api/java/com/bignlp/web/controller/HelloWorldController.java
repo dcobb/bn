42bed07fd536fea0ee0c84ac8f5b77ba0e2cdde1
athondapu <athondapu@ciphercloud.com>
2012-11-01 18:00:30 +0530

    modified controller code...

--- com.bignlp.web/src/api/java/com/bignlp/web/controller/HelloWorldController.java
+++ com.bignlp.web/src/api/java/com/bignlp/web/controller/HelloWorldController.java
@@ -6,41 +6,79 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bignlp.web.model.Converter;
+
 @Controller
-// @RequestMapping(value = "/helloworld")
 @RequestMapping("/hello")
 public class HelloWorldController {
 
-	@RequestMapping(value = "{argAppContextPath}", method = RequestMethod.GET)
-	public String displayLayout(Model argModel,
-			@PathVariable String argAppContextPath,
-			HttpServletRequest argRequest, HttpServletResponse argResponse) {
-		if (argAppContextPath != null && !argAppContextPath.isEmpty()) {
-			argModel.addAttribute("contextPath", argAppContextPath);
-		} else {
-			argModel.addAttribute("contextPath", "ROOT");
-		}
-		return "helloworld";
-	}
+	private static final String FILE_EXTENSION = ".txt";
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	/*
+	 * Here argFileName should be there in your testdata folder. Output of this
+	 * method is in the form of String.
+	 */
+	@RequestMapping(value = "{argFileName}", method = RequestMethod.GET)
 	public @ResponseBody
-	InputStream printHello() {
-		File inputFile = new File(this.getClass().getResource("/example.txt")
-				.getFile());
+	String printHello(@PathVariable("argFileName") String argFileName) {
+		File inputFile = new File(this.getClass()
+				.getResource(buildFilePath(argFileName)).getFile());
 		InputStream fileInputStream = null;
 		try {
 			fileInputStream = new FileInputStream(inputFile);
+			return IOUtils.toString(fileInputStream);
+			// return fileInputStream;
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("File not found: ", e);
+		} catch (IOException e) {
+			throw new RuntimeException(
+					"Exception while converting from inputstream to string: ",
+					e);
+		} finally {
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (Exception ignore) {
+					// ignore
+				}
+			}
+		}
+	}
+
+	private String buildFilePath(String argFileName) {
+		if (argFileName != null && !argFileName.isEmpty()) {
+			return "/" + argFileName + FILE_EXTENSION;
+		}
+		return argFileName;
+	}
+
+	/*
+	 * Here argFileName should be there in your testdata folder. Output of this
+	 * method is in the form of JSON.
+	 */
+	@RequestMapping(value = "/json/{argFileName}", method = RequestMethod.GET)
+	public @ResponseBody
+	Converter printJson(@PathVariable("argFileName") String argFileName) {
+		File inputFile = new File(this.getClass()
+				.getResource(buildFilePath(argFileName)).getFile());
+		InputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(inputFile);
+			try {
+				Converter converter = new Converter();
+				converter.setBody(IOUtils.toString(fileInputStream));
+				return converter;
+			} catch (IOException e) {
+				throw new RuntimeException(
+						"Unable to convert inputstream to string: ", e);
+			}
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException("File not found: ", e);
 		} finally {
@@ -52,7 +90,6 @@ public class HelloWorldController {
 				}
 			}
 		}
-		return fileInputStream;
 	}
 
 }
---
+++ com.bignlp.web/src/api/java/com/bignlp/web/model/Converter.java
@@ -0,0 +1,19 @@
+package com.bignlp.web.model;
+
+public class Converter {
+
+	private String body;
+
+	public Converter() {
+		super();
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
